bafff3f1f855817c4a3a38bb7dd9d3cbc761d7fb
Phan Văn Hoàng Anh <pvhoanganh240@gmail.com>
2024-07-26 16:24:21 +0700

    feat: updating login form

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
+// _layout.tsx
 import { Stack } from "expo-router";
+import React from "react";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+    return (
+        <Stack>
+            <Stack.Screen name="index" />
+        </Stack>
+    );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,9 @@
-import { Text, View } from "react-native";
+// index.tsx
+import React from "react";
+import AppNavigator from "../components/navigation/AppNavigator";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+const Index = () => {
+    return <AppNavigator />;
 }
+
+export default Index;
