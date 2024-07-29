b9f30dfc65da022b465f1555facd20aaadbb9d47
Phan Văn Hoàng Anh <pvhoanganh240@gmail.com>
2024-07-29 15:50:22 +0700

    feat: ref

--- app/components/screens/LoginScreen.tsx
+++ app/components/screens/LoginScreen.tsx
@@ -35,7 +35,7 @@ const LoginScreen = () => {
     
                 if (response.ok) {
                     Alert.alert('Success', 'Login successful, navigating to Home.');
-                    router.replace('/HomeScreen');
+                    router.replace('/components/screens/HomeScreen');
                 } else {
                     if (data.errors && data.errors.email) {
                         Alert.alert('Login Failed', data.errors.email);
